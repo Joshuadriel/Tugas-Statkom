9dd6bfb888efbe400fa99e7604c878566178db25
Joshuadriel <joshuadriel.suhandi@gmail.com>
2026-06-09 12:53:00 +0700

    Update input dan sorting array

--- A.java
+++ A.java
@@ -1,8 +1,44 @@
 import java.util.Scanner;
 public class A {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    /**
+     * Sorting array dari yang terkecil ke terbesar
+     * @param arr = kumpulan data yang ingin diolah
+     * @return arr yang sudah disorting kembali
+     */
+    public static double[] sorting(double arr[])
+    {
+        // Metode sorting: Selection sort
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            double min = arr[i];
+            int pos = i;
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[j] < min)
+                {
+                    min = arr[j];
+                    pos = j;
+                }
+            }
+            double temp = arr[i];
+            arr[i] = min;
+            arr[pos] = temp;
+        }
 
-        sc.close();
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Import scanner
+        double arr[] = new double[24]; // Array untuk menyimpan nilai-nilai datanya
+        // Looping untuk memasukkan setiap data ke dalam array
+        for (int i = 0; i < 24; i++)
+        {
+            arr[i] = sc.nextDouble();
+        }
+
+        // Mengolah data
+        arr = sorting(arr); // Menyortir array terlebih dahulu dari yang terkecil ke terbesar
+
+        sc.close(); // Menutup scanner;
     }
 }
